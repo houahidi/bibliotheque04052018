78cc9e1ddb68c2ac5d844bc833e866e279f059d3
houahidi <houahidi@uni-consultig.fr>
2018-06-05 15:35:13 +0100

    test template

---
+++ UniLibraryWeb-vdb/src/test/java/fr/uni/institute/library/dao/CategoryDaoTestCase.java
@@ -0,0 +1,24 @@
+package fr.uni.institute.library.dao;
+
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+class CategoryDaoTestCase {
+
+	@Before
+	void setUp() throws Exception {
+	}
+
+	@After
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testResearchAllCategories() {
+		fail("Test de recherche des categories n'est pas encre implemente"); // TODO
+	}
+
+}
