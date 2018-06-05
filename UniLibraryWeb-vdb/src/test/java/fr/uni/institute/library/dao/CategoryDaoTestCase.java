092565a8b44ddb25cfa9ebcf5c7343bd7513a012
houahidi <houahidi@uni-consultig.fr>
2018-06-05 15:53:46 +0100

    add CategoryTestCase

--- UniLibraryWeb-vdb/src/test/java/fr/uni/institute/library/dao/CategoryDaoTestCase.java
+++ UniLibraryWeb-vdb/src/test/java/fr/uni/institute/library/dao/CategoryDaoTestCase.java
@@ -1,24 +1,37 @@
 package fr.uni.institute.library.dao;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-class CategoryDaoTestCase {
+public class CategoryDaoTestCase {
+	
+	private static Logger logger = Logger.getLogger( CategoryDaoTestCase.class);
+	private int nombreAttendu;
 
 	@Before
-	void setUp() throws Exception {
+	public void setUp() throws Exception {
+		logger.info("Initialisation des ressources necessaires avant le lancement de test");
+		//chargement de fichiers xml ou autre pour recuperer les resultat attendu
+		nombreAttendu = 9;
+		
 	}
 
 	@After
-	void tearDown() throws Exception {
+	public void tearDown() throws Exception {
+		logger.info("Libération des ressources utilisees dans test");
 	}
 
-	@Test
-	void testResearchAllCategories() {
-		fail("Test de recherche des categories n'est pas encre implemente"); // TODO
+	@Test()
+	public void testResearchAllCategories() {
+		logger.info("Execution de test ResearchAllCategories");
+		int nombreCalcule = 9;
+		assertEquals(nombreAttendu, nombreCalcule);
+		//fail("Test de recherche des categories n'est pas encre implemente"); // TODO
 	}
 
 }
